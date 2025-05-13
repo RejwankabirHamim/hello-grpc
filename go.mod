616144e37fe312467162e5fae4a1091bfb257f19
RejwankabirHamim <hamim@appscode.com>
2025-05-13 11:47:51 +0600

    final commit

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module github.com/RejwankabirHamim/hello-grpc
 
-go 1.22.10
+go 1.23.0
+
+toolchain go1.24.2
+
+require (
+	google.golang.org/grpc v1.72.0
+	google.golang.org/protobuf v1.36.6
+)
+
+require (
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250505200425-f936aa4a68b2 // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-  "fmt"
+	"context"
+	"github.com/RejwankabirHamim/hello-grpc/invoicer"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+type myInvoicerServer struct {
+	invoicer.UnimplementedInvoicerServer
+}
+
+func (s myInvoicerServer) Create(ctx context.Context, req *invoicer.CreateRequest) (*invoicer.CreateResponse, error) {
+	return &invoicer.CreateResponse{
+		Pdf:  []byte(req.From),
+		Docx: []byte("test"),
+	}, nil
+}
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
-
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("cannot create listener: %s", err)
+	}
+	severRegistrar := grpc.NewServer()
+	service := &myInvoicerServer{}
+	invoicer.RegisterInvoicerServer(severRegistrar, service)
+	if err := severRegistrar.Serve(lis); err != nil {
+		log.Fatalf("cannot serve: %s", err)
+	}
 }
